a03deb0f95ad49cc0a56d6bd13d29244fc3b04f0
yanyu <3303295829@qq.com>
2023-08-24 10:09:47 +0800

    job

---
+++ src/main/java/com/yanyu/init/esdao/PostEsDao.java
@@ -0,0 +1,14 @@
+package com.yanyu.init.esdao;
+
+import com.yanyu.init.model.dto.post.PostEsDTO;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+/**
+ * 帖子 ES 操作
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
--- src/main/java/com/yanyu/init/mapper/PostMapper.java
+++ src/main/java/com/yanyu/init/mapper/PostMapper.java
@@ -3,6 +3,13 @@ package com.yanyu.init.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yanyu.init.model.entity.Post;
 
+import java.util.Date;
+import java.util.List;
+
 public interface PostMapper extends BaseMapper<Post> {
 
+    /**
+     * 查询帖子列表（包括已被删除的数据）
+     */
+    List<Post> listPostWithDelete(Date minUpdateTime);
 }
